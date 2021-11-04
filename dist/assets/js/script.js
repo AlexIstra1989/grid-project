c71610d37eb70dc9ad85ec7df9f752cc4f1d121b
AlexIstra1989 <alex888fcsm@gmail.com>
2021-11-04 22:39:09 +0300

    fix bug

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -1,8 +1,22 @@
 
 $(function() {
+ $(".menu").on("click", "a", function (event) {
+  event.preventDefault();
+  var id = $(this).attr('href'),
+    top = $(id).offset().top;
+  $('body,html').animate({ scrollTop: top }, 1000);
+});
+
   $('.menu__burger').on('click', function () {
     $('.menu').toggleClass('menu--active');
+    $('.menu__burger').toggleClass('menu__burger--active');
   })
+
+  $('.menu__burger').click(function (event) {
+   event.preventDefault();
+   $('body').toggleClass('overflow');
+ });
 });
+
 
 //# sourceMappingURL=script.js.map
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,19 @@
 $(function() {
+ $(".menu").on("click", "a", function (event) {
+  event.preventDefault();
+  var id = $(this).attr('href'),
+    top = $(id).offset().top;
+  $('body,html').animate({ scrollTop: top }, 1000);
+});
+
   $('.menu__burger').on('click', function () {
     $('.menu').toggleClass('menu--active');
+    $('.menu__burger').toggleClass('menu__burger--active');
   })
+
+  $('.menu__burger').click(function (event) {
+   event.preventDefault();
+   $('body').toggleClass('overflow');
+ });
 });
+
